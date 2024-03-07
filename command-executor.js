5bf15f77738e7ad89ad5847dea8b444a260c091d
redaphid <iam@aaronherres/com>
2024-03-07 11:19:12 -0700

    add nvmrc

--- command-executor.js
+++ command-executor.js
@@ -1,8 +1,10 @@
-import {execSync} from 'child_process';
+import {exec} from 'child_process';
 const KEY_DOWN = 160
 const KEY_UP = 128
 
 const FIRST_BIG_BUTTON = 92
+const SECOND_BIG_BUTTON = 93
+const THIRD_BIG_BUTTON = 94
 export class CommandExecutor {
 
   registerCommand(command, executor) {
@@ -10,6 +12,7 @@ export class CommandExecutor {
   }
 
   executeCommand({button, event, pressure, raw}) {
+    console.log(button)
     switch(button){
       case FIRST_BIG_BUTTON: {
         switch (event) {
@@ -17,12 +20,38 @@ export class CommandExecutor {
             console.log('Button 1 pressed');
             if(pressure >= 127) {
               console.log('Button 1 pressed hard');
-              execSync('open -n /Applications/Google\\ Chrome.app');
+              exec('open -n /Applications/Google\\ Chrome.app');
             }
             return;
           }
-          case KEY_UP: {
-            console.log('Button 1 released');
+          default: {
+            return;
+          }
+        }
+      }
+      case SECOND_BIG_BUTTON: {
+        switch (event) {
+          case KEY_DOWN: {
+            console.log('Button 2 pressed');
+            if(pressure >= 127) {
+              console.log('Button 2 pressed hard');
+              exec('open /Applications/Alacritty.app -n;');
+            }
+            return;
+          }
+          default: {
+            return;
+          }
+        }
+      }
+      case THIRD_BIG_BUTTON: {
+        switch (event) {
+          case KEY_DOWN: {
+            console.log('Button 3 pressed');
+            if(pressure >= 127) {
+              console.log('Button 3 pressed hard');
+              exec('code');
+            }
             return;
           }
           default: {
