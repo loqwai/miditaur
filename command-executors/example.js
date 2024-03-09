289d14b5f88234a604f7b6e1d522445df94e4d27
Aaron Herres <iam@aaronherres.com>
2024-03-09 15:03:08 -0700

    allow for more than one configured executor

---
+++ command-executors/example.js
@@ -0,0 +1,33 @@
+import { exec } from 'child_process';
+
+const KEY_DOWN = 160;
+const FIRST_BIG_BUTTON = 92;
+const SECOND_BIG_BUTTON = 93;
+const THIRD_BIG_BUTTON = 94;
+const FOURTH_BIG_BUTTON = 95;
+export class CommandExecutor {
+  executeCommand({ button, event, pressure }) {
+    if (event !== KEY_DOWN || pressure < 127) return;
+
+    if (button === FIRST_BIG_BUTTON) {
+      exec('open -n /Applications/Google\\ Chrome.app');
+      return;
+    }
+
+    if (button === SECOND_BIG_BUTTON) {
+      exec('open /Applications/Alacritty.app -n;');
+      return;
+    }
+
+    if (button === THIRD_BIG_BUTTON) {
+      exec('code');
+      return;
+    }
+    if (button === FOURTH_BIG_BUTTON) {
+      exec('open /Applications/Slack.app -n;');
+      return;
+    }
+  }
+}
+
+export default CommandExecutor;
--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 #!/usr/bin/env node
 import midi from 'midi';
-import CommandExecutor from './command-executor.js';
-const executor = new CommandExecutor();
-// Open and listen to all MIDI input ports
-
-const openAndListenToAllPorts = () => {
+const listenToMidi = (executor) => {
     const numPorts = new midi.input().getPortCount();
+    console.log(`Found ${numPorts} MIDI input ports`);
     for (let i = 0; i < numPorts; i++) {
         const input = new midi.input();
         input.openPort(i);
         input.on('message', (deltaTime, message) => {
+            console.log(`MIDI message received: ${message}`);
             executor.executeCommand({button: message[1], event: message[0], pressure: message[2], raw: message});
         });
     }
 };
 
 // Main function
-const main = () => {
+const main = async (executorPath) => {
     console.log('Starting...');
-    openAndListenToAllPorts();
+    executorPath = executorPath || './command-executors/example.js';
+    console.log(`Using executor at ${executorPath}`);
+    const executorModule = await import(executorPath);
+    const executor = new executorModule.default();
+    listenToMidi(executor);
     // Add more logic here if needed
 };
 
 // Run main function
-main();
+main(...process.argv.slice(2));
