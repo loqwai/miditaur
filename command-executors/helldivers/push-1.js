573d7c6f665e7ac4f1c019620cbef7d19774e379
Aaron Herres <iam@aaronherres.com>
2024-03-09 18:25:34 -0700

    use nutjs instead of robotjs bc robotjs appears to be broken

--- command-executors/helldivers/push-1.js
+++ command-executors/helldivers/push-1.js
@@ -1,62 +1,65 @@
+import {keyboard, Key} from '@nut-tree/nut-js';
 import debounce from "debounce";
-import robot from "robotjs";
+
+
 const macros = {
   92: {
     name: "Helldivers: Machine Gun",
-    keys: "down left down up right",
+    keys: [Key.Down, Key.Left, Key.Down, Key.Up, Key.Right],
   },
   93: {
     name: "test",
-    keys: '',
-  }
+    keys: [Key.W, Key.W, Key.W, Key.W, Key.W],
+  },
 };
+
 const KEY_PRESSED = 160;
 
 export class CommandExecutor {
   constructor(input, output) {
     this.input = input;
     this.output = output;
-    this.keydown = debounce(this.keydown, 100);
+    this.keydown = debounce(this.keydown.bind(this), 100);
     this.setAllColors();
+    keyboard.config.autoDelayMs = 5;
   }
 
-  keydown(button) {
+  async keydown(button) {
     const macro = macros[button];
-    console.log({button});
+    console.log({ button });
     if (!macro) return;
     console.log(`Executing: ${macro.name}`);
-    const keys = macro.keys.split(" ");
-    keys.forEach((key) => {
-      if(!key) return;
-      robot.keyTap(key);
-    });
+    const keys = macro.keys;
+    for (const key of keys) {
+      await keyboard.pressKey(key);
+      await keyboard.releaseKey(key);
+    }
     this.output.sendMessage([144, button, 127]);
   }
 
-  executeCommand({ button, event, pressure }) {
-    if(event === KEY_PRESSED) return this.keydown(button);
+  async executeCommand({ button, event, pressure }) {
+    if (event === KEY_PRESSED) return this.keydown(button);
   }
 
-  setAllColors() {
-    console.log('Setting all colors');
+  async setAllColors() {
+    console.log("Setting all colors");
     // Simplifying the color scheme to solid, bright colors for each quadrant
     const colors = [
-      5,  // Bright Red
+      5, // Bright Red
       21, // Bright Green
       41, // Bright Ocean
-      53  // Bright Pink
+      53, // Bright Pink
     ];
     for (let x = 0; x < 8; x++) {
       for (let y = 0; y < 8; y++) {
-        const button = 92 + x - (y * 8);
+        const button = 92 + x - y * 8;
         // Determine the quadrant to assign the color
         const quadrant = Math.floor(x / 4) + Math.floor(y / 4) * 2;
         const color = colors[quadrant];
-        this.output.sendMessage([144, button, color]);
+        await this.output.sendMessage([144, button, color]);
       }
     }
   }
-
 }
 
 export default CommandExecutor;
